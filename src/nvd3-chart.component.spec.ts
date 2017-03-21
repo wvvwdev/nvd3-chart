ee506295390fbeb68ba261379ef60e6235c70c23
Diabeu <scibor.rudnicki@ster-project.pl>
2017-03-21 15:58:20 +0100

    test: change component name to CamelCase

--- src/nvd3-chart.component.spec.ts
+++ src/nvd3-chart.component.spec.ts
@@ -1,30 +1,31 @@
+
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { ComponentFixture, TestBed, ComponentFixtureAutoDetect, async, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { nvd3ChartComponent } from './nvd3-chart.component';
+import { Nvd3ChartComponent } from './nvd3-chart.component';
 
 TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 describe('nvd3ChartComponent', () => {
 
-  let comp: nvd3ChartComponent;
-  let fixture: ComponentFixture<nvd3ChartComponent>;
+  let comp: Nvd3ChartComponent;
+  let fixture: ComponentFixture<Nvd3ChartComponent>;
   let de: DebugElement;
   let el: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ 
-        nvd3ChartComponent 
+      declarations: [
+        Nvd3ChartComponent
       ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(nvd3ChartComponent);
+    fixture = TestBed.createComponent(Nvd3ChartComponent);
     comp = fixture.componentInstance; // to access properties and methods
-    de = fixture.debugElement; // test helper 
+    de = fixture.debugElement; // test helper
     el = fixture.nativeElement; // to access DOM element
   });
 
